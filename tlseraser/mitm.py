f691d781239e12e96e8a1a055bb2c4361c6ca4d0
Adrian Vollmer <adrian.vollmer@syss.de>
2019-04-29 17:10:32 +0200

    fix exception order

--- tlseraser/mitm.py
+++ tlseraser/mitm.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3
+#  Copyright (c) 2019 Adrian Vollmer
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to permit
+#  persons to whom the Software is furnished to do so, subject to the
+#  following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+#  NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#  USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
 This module opens the main sockets. Incoming connections are relayed to
 a "socket mirror" and then to their original destination, assuming they have
@@ -164,15 +186,15 @@ class Forwarder(threading.Thread):
                 return
             else:
                 data = conn.recv(1024)
-        except (ConnectionResetError, OSError):
-            log.debug("[%s] Connection reset: %s" % (self.id, conn))
-            self.disconnect(conn)
-            return
         except ssl.SSLWantReadError:
             # can be ignored. data will be read next time
             return False
         except ssl.SSLError:
             log.exception("[%s] Exception while reading" % self.id)
+            return False
+        except (ConnectionResetError, OSError):
+            log.debug("[%s] Connection reset: %s" % (self.id, conn))
+            self.disconnect(conn)
             return False
         if data:
             #  log.debug('Read %d bytes' % len(data))
